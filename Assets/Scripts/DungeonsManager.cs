ae34ca0f440ad3cc5633c9a8b434e16e7dbbb019
TheSalamek <thesalamek@seznam.cz>
2023-10-16 15:29:01 +0200

    xd

--- Assets/Scripts/DungeonsManager.cs
+++ Assets/Scripts/DungeonsManager.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DungeonsManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Button buttonAttack;
+    private void ShowAttackPanel()
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+        // Show the UI panel with the stats and options
+        uiManager.panelEquipOrSell.SetActive(true);
+        // Show the stats of newly generated item
+        uiManager.ShowNewItemText(newItem);
+        // Show the stats of the already equipped item
+        uiManager.ShowExistingItemText(existingItemSlotIndex);
+
+        // Remove any existing listeners first
+        buttonAttack.onClick.RemoveAllListeners();
+
+        // Add button click events to handle player choice
+        buttonAttack.onClick.AddListener(() => Attack());
+    }
+    private void Attack()
     {
-        
+
     }
 }
