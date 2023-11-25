d98e9b5b88e5769fd64d708c43b34f45db17d597
TheSalamek <thesalamek@seznam.cz>
2023-11-25 22:49:48 +0100

    Combat Log Update

--- Assets/Scripts/BattleManager.cs
+++ Assets/Scripts/BattleManager.cs
@@ -1,8 +1,11 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 
 public class BattleManager : MonoBehaviour
@@ -20,6 +23,11 @@ public class BattleManager : MonoBehaviour
     private bool enemyFrozen = false;
     private int playerOnFire = 0;
     private int enemyOnFire = 0;
+
+    [SerializeField] private GameObject battleLog;
+    [SerializeField] private GameObject panelTapToClose;
+    [SerializeField] private GameObject battleLogEntryPrefab;
+    [SerializeField] private Transform battleLogContent;
 
     [SerializeField] public GameObject panelWin;
     [SerializeField] public GameObject panelLose;
@@ -52,7 +60,7 @@ public class BattleManager : MonoBehaviour
             main.LoadPlayerData();
             enemyStats = SetEnemyStats(1, main.currentStage);
 
-            // Player stats
+            // set Players stats
             playerStats.damage = main.damage;
             playerStats.health = main.health;
             playerStats.speed = main.speed;
@@ -61,10 +69,8 @@ public class BattleManager : MonoBehaviour
             playerStats.comboChance = main.comboChance;
             playerStats.fireChance = main.fireChance;
             playerStats.freezeChance = main.freezeChance;
-            Debug.Log("Player dmg " + playerStats.damage);
 
-            // Enemy stats
-            Debug.Log("Enemy dmg " + enemyStats.damage);
+            // Determine who starts first
             if (playerStats.speed < enemyStats.speed)
             {
                 playerTurn = false;
@@ -79,21 +85,25 @@ public class BattleManager : MonoBehaviour
 
     private void BattleStarted()
     {
-        Debug.Log("Battle Started");
         System.Random random = new System.Random();
-
         int turn = 1;
+
+        int enemyDamage = enemyStats.damage - playerStats.resistance;
+        int playerDamage = playerStats.damage - enemyStats.resistance;
 
         battleOngoing = true;
         while (battleOngoing == true && turn <= 20)
         {
+            AddEntry(string.Format("<align=center>Turn {0}</align>", turn));
             Debug.Log(turn + " turn");
             if (playerTurn) // Player turn
             {
-                Debug.Log("Player turn");
                 if (playerOnFire > 0) // Player is on fire
                 {
-                    playerStats.health = playerStats.health - enemyStats.damage / 10;
+                    int fireDamage = enemyStats.damage / 10;
+                    playerStats.health -= fireDamage;
+                    AddEntry(string.Format("Turn {0}: <color=green>Player</color> took {1} <color=orange>fire</color> DMG", turn, fireDamage));
+                    AddEntry(string.Format("Turn {0}: <color=green>Player</color> has <color=#ff5050>{1} HP</color> remaining", turn, playerStats.health));
                     playerOnFire--;
                 }
 
@@ -106,6 +116,7 @@ public class BattleManager : MonoBehaviour
                 if (playerFrozen == true) // Player is frozen
                 {
                     playerFrozen = false;
+                    AddEntry(string.Format("Turn {0}: <color=green> Player </color> is <color=lightblue> frozen </color>", turn));
                 }
                 else
                 {
@@ -115,20 +126,29 @@ public class BattleManager : MonoBehaviour
                         randomNumberCounter = random.Next(1, 101);
                         if (randomNumberFire <= playerStats.fireChance && enemyOnFire < 3) // Player sets Enemy on fire
                         {
+                            AddEntry(string.Format("Turn {0}: <color=green>Player</color> set <color=red>Enemy</color> on <color=orange>fire</color>", turn));
                             enemyOnFire = 3;
                         }
 
                         if (randomNumberFreeze <= playerStats.freezeChance && enemyFrozen == false) // Player freezes Enemy
                         {
+                            AddEntry(string.Format("Turn {0}: <color=green>Player</color> <color=lightblue>froze </color> <color=red>Enemy</color>", turn));
                             enemyFrozen = true;
                         }
 
                         //Attack enemy
-                        enemyStats.health -= (playerStats.damage - enemyStats.resistance);
+                        
+                        enemyStats.health -= playerDamage;
+                        AddEntry(string.Format("Turn {0}: <color=green>Player</color> delt {1} DMG", turn, playerDamage));
+                        AddEntry(string.Format("Turn {0}: <color=red>Enemy</color> has <color=#ff5050>{1} HP</color> remaining",turn, enemyStats.health));
+
                         //Check if enemy Counters
                         if (randomNumberCounter <= enemyStats.counterChance)
                         {
-                            playerStats.health -= (enemyStats.damage - playerStats.resistance);
+                            
+                            playerStats.health -= enemyDamage;
+                            AddEntry(string.Format("Turn {0}: <color=red>Enemy</color> Countered and delt {1} DMG", turn, enemyDamage));
+                            AddEntry(string.Format("Turn {0}: <color=green>Player</color> has <color=#ff5050>{1} HP</color> remaining", turn, playerStats.health));
                         }
 
                     } while (randomNumberCombo <= playerStats.comboChance && enemyStats.health > 0);
@@ -138,10 +158,12 @@ public class BattleManager : MonoBehaviour
             }
             else //Enemy turn
             {
-                Debug.Log("Enemy turn");
                 if (enemyOnFire > 0) // Enemy is on fire
                 {
-                    enemyStats.health -= playerStats.damage / 10;
+                    int fireDamage = playerStats.damage / 10;
+                    enemyStats.health -= fireDamage;
+                    AddEntry(string.Format("Turn {0}: <color=red>Enemy</color> took {1} <color=orange>fire</color> DMG", turn, fireDamage));
+                    AddEntry(string.Format("Turn {0}: <color=red>Enemy</color> has <color=#ff5050>{1} HP</color> remaining", turn, enemyStats.health));
                     enemyOnFire--;
                 }
 
@@ -154,6 +176,7 @@ public class BattleManager : MonoBehaviour
                 if (enemyFrozen == true) // Enemy is frozen
                 {
                     enemyFrozen = false;
+                    AddEntry(string.Format("Turn {0}: <color=red>Enemy</color> is <color=lightblue>frozen</color>", turn));
                 }
                 else
                 {
@@ -164,19 +187,25 @@ public class BattleManager : MonoBehaviour
                         if (randomNumberFire <= enemyStats.fireChance && playerOnFire < 3) // Enemy sets player on fire
                         {
                             playerOnFire = 3;
+                            AddEntry(string.Format("Turn {0}: <color=red>Enemy</color> set <color=green>Player</color> on <color=orange>fire</color>", turn));
                         }
 
                         if (randomNumberFreeze <= enemyStats.freezeChance && playerFrozen == false) // Enemy freezes Player
                         {
                             playerFrozen = true;
+                            AddEntry(string.Format("Turn {0}: <color=red>Enemy</color> <color=lightblue>froze</color> <color=green>Player</color>", turn));
                         }
 
                         //Attack player
-                        playerStats.health -= (enemyStats.damage - playerStats.resistance);
+                        playerStats.health -= enemyDamage;
+                        AddEntry(string.Format("Turn {0}: <color=red>Enemy</color> delt {1} DMG", turn, enemyDamage));
+                        AddEntry(string.Format("Turn {0}: <color=green>Player</color> has <color=#ff5050>{1} HP</color> remaining", turn, playerStats.health));
                         //Check if player Counters
                         if (randomNumberCounter <= playerStats.counterChance)
                         {
                             enemyStats.health -= (playerStats.damage - enemyStats.resistance);
+                            AddEntry(string.Format("Turn {0}: <color=green>Player</color> Countered and delt {1} DMG", turn, playerDamage));
+                            AddEntry(string.Format("Turn {0}: <color=red>Enemy</color> has <color=#ff5050>{1} HP</color> remaining", turn, enemyStats.health));
                         }
 
                     } while (randomNumberCombo <= enemyStats.comboChance && playerStats.health > 0);
@@ -210,10 +239,12 @@ public class BattleManager : MonoBehaviour
         Debug.Log("Battle Lost");
         battleOngoing = false;
         panelLose.SetActive(true);
+        AddEntry(string.Format("Battle Lost"));
     }
     private void BattleWon()
     {
         Debug.Log("Battle Won");
+        AddEntry(string.Format("Battle Won!"));
         panelWin.SetActive(true);
         main.hourglass += enemyStats.hourglass;
         battleOngoing = false;
@@ -227,6 +258,34 @@ public class BattleManager : MonoBehaviour
         }
         main.SavePlayerData();
     }
+
+    public void ShowBattleLog()
+    {
+        panelTapToClose.SetActive(true);
+        battleLog.SetActive(true);
+    }
+
+    public void CloseBattleLog()
+    {
+        panelTapToClose.SetActive(false);
+        battleLog.SetActive(false);
+    }
+
+    private void AddEntry(string message)
+    {
+        GameObject entry = Instantiate(battleLogEntryPrefab, battleLogContent);
+        entry.GetComponent<TextMeshProUGUI>().text = message;
+
+        //ScrollToBottom();
+    }
+
+    private void ScrollToBottom()
+    {
+        // Assuming logContent.parent has a ScrollRect component
+        ScrollRect scrollRect = battleLogContent.parent.GetComponent<ScrollRect>();
+        scrollRect.normalizedPosition = new Vector2(0, 0);
+    }
+
     public EnemyStatsSO SetEnemyStats(int island, int stage)
     {
         EnemyStatsSO enemy = EnemyDatabase.instance.GetEnemy(island, stage);
